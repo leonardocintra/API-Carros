384415a3a81b4c0e861ae6b71a91f340988f6772
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-10-20 22:34:17 -0200

    Add class Carro

---
+++ src/br/com/livro/domain/BaseDAO.java
@@ -0,0 +1,31 @@
+package br.com.livro.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+	
+	public BaseDAO() {
+		try{
+			// Necessário para utilizar o driver JDBC do MySQL
+			Class.forName("com.mysql.jdbc.driver");
+		}
+		catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+
+	protected Connection getConnection() throws SQLException {
+		String url = "jdbc:mysql://localhost/livro";
+		Connection conn = DriverManager.getConnection(url, "livro", "livro123");
+		return conn;
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		BaseDAO db = new BaseDAO();
+		Connection conn = db.getConnection();
+		// testa a conexão;
+		System.out.println(conn);
+	}
+}
