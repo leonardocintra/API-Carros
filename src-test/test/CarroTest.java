9828d83f25b1c791486835c828bdec6d1ecf970b
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-10-21 20:54:24 -0200

    Add test CRUD Carros

--- src-test/test/CarroTest.java
+++ src-test/test/CarroTest.java
@@ -22,8 +22,36 @@ public class CarroTest extends TestCase {
 		assertEquals("Ferrari FF", ferrari.getNome());
 		// valida se encontrou a Bugatti
 		Carro bugatti = carroService.findByName("Bugatti Veyron").get(0);
-		assertEquals("Bugatti Veyron", bugatti.getNome());
-		
-		
+		assertEquals("Bugatti Veyron", bugatti.getNome());		
+	}
+	
+	public void testSalvarDeletarCarro(){
+		Carro c = new Carro();
+		c.setNome("Teste");
+		c.setDesc("Teste desc");
+		c.setUrlFoto("url foto aqui");
+		c.setUrlFoto("url vidio aqui");
+		c.setLatitude("la3829389");
+		c.setLongitude("loi382938");
+		c.setTipo("que tipo");
+		carroService.save(c);
+		// id do carro salvo
+		Long id = c.getId();
+		assertNotNull(id);
+		// busca no banco de dados para confirmar se o carro foi salvo
+		c = carroService.getCarro(id);
+		assertEquals("Teste", c.getNome());
+		assertEquals("la3829389", c.getLatitude());
+		// atualiza o carro
+		c.setNome("Teste UPDATE");
+		carroService.save(c);
+		c = carroService.getCarro(id);
+		assertEquals("Teste UPDATE", c.getNome());
+		assertEquals("la3829389", c.getLatitude());
+		// deleta o carro
+		carroService.delete(id);
+		// busca o carro novamente
+		c = carroService.getCarro(id);
+		assertNull(c);	
 	}
 }
