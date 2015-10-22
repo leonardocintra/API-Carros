2860d48018f752e6be55c1ea2fda6417ccb07053
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-10-22 21:01:23 -0200

    Configurado o Jersey

---
+++ src/br/com/livro/rest/MyApplication.java
@@ -0,0 +1,29 @@
+package br.com.livro.rest;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.jettison.JettisonFeature;
+
+public class MyApplication extends Application {
+	
+	@Override
+	public Set<Object> getSingletons() {
+		Set<Object> singletons = new HashSet<>();
+		// Driver do Jettison para gerar Json
+		singletons.add(new JettisonFeature());
+		return singletons;
+	}
+	
+	@Override
+	public Map<String, Object> getProperties() {
+		Map<String, Object> properties = new HashMap<>();
+		// configura o pacote para fazer scan das classes con anotacao REST.
+		properties.put("jersey.config.server.provider.packages", "br.com.livro");
+		return properties;
+	}
+}
